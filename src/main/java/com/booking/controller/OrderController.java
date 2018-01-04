3379fae31949d8b2ea78b1be30ab7ff2e0395797
w8forme <velesgp@mail.ru>
2018-01-04 16:26:04 +0200

    Added order service test

--- src/main/java/com/booking/controller/OrderController.java
+++ src/main/java/com/booking/controller/OrderController.java
@@ -43,4 +43,6 @@ public class OrderController {
         System.out.println(seats);
         return "redirect:tickets";
     }
+
+
 }
---
+++ src/test/java/com/booking/service/impl/OrderServiceImplTest.java
@@ -0,0 +1,71 @@
+package com.booking.service.impl;
+
+import com.booking.model.Order;
+import com.booking.model.Seat;
+import com.booking.repository.OrderRepository;
+import com.booking.repository.SeatRepository;
+import com.booking.service.OrderService;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrderServiceImplTest {
+
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private SeatRepository seatRepository;
+    @Autowired
+    private OrderRepository orderRepository;
+
+    private Order orderForTest;
+
+    private Seat seatForTest;
+
+    @Test
+    public void deleteAllOrders() {
+    }
+
+    @Test
+    public void makeOrder() {
+        orderService.makeOrder(orderForTest);
+        assertThat(orderRepository.getOne(orderForTest.getId())).isNotNull();
+    }
+
+    @Test
+    public void getAllOrders() {
+        orderRepository.save(orderForTest);
+        List<Order> orders = orderService.getAllOrders();
+        assertThat(orders.isEmpty()).isFalse();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        orderForTest = new Order();
+        seatForTest = seatRepository.findOne(1L);
+        orderForTest.setEmail("test@example.com");
+        orderForTest.setName("Alex");
+        orderForTest.setTotal(seatForTest.getPrice());
+        List<Seat> seats = new ArrayList<>();
+        seats.add(seatForTest);
+        orderForTest.setSeatsList(seats);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        orderService.deleteAllOrders();
+    }
+}
